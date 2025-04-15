3ecd30b04c0870463fc8589497ef6331bf6d19e8
mikel <mikelreza@gmail.com>
2025-04-14 20:15:48 -0700

    changed background of image

--- Screen1.py
+++ Screen1.py
@@ -7,7 +7,7 @@ from pyglet.window.key import KeyStateHandler
 
 class ScreenOne:
     batch = pyglet.graphics.Batch()
-
+    background_image = pyglet.resource.image("sheetmetal_fancy.jpg")
 
     def __init__(self, window):
         self.progress = 0
@@ -15,13 +15,16 @@ class ScreenOne:
 
         self.window = window
 
+        self.background_image.width = 1300
+        self.background_image.height = 700
+
         self.bar_height = 30
         self.bar_width = window.width - 80
         self.bar_x = (window.width - self.bar_width) / 2
         self.bar_y = window.height - 100
 
         self.bar_main_background = pyglet.shapes.Rectangle(self.bar_x, self.bar_y, self.bar_width, self.bar_height,
-                                               color=(50, 50, 50), batch=self.batch)
+                                               color=(0, 0, 0), batch=self.batch)
         self.bar_main_foreground = pyglet.shapes.Rectangle(self.bar_x, self.bar_y, 20, self.bar_height,
                              color=(0, 200, 0), batch=self.batch)
 
@@ -37,6 +40,7 @@ class ScreenOne:
         @window.event
         def on_draw():
             self.window.clear()
+            self.background_image.blit(-280, 0)
             self.update_bars()
             self.batch.draw()
 
